7d6c82c523c10dc3870b7422ac7051539a00b8fa
Kristjan Rajaniemi <kristjan.rajaniemi@gmail.com>
2026-09-17 23:59:08 +0300

    bun version

--- src/utils/functions/getSysInfo.ts
+++ src/utils/functions/getSysInfo.ts
@@ -24,7 +24,7 @@ const getSysInfo = async () => {
         startupTime: new Date(),
         os_version: await getOSVersion(),
         bot_version: `v${await getBotVersion()}`,
-        runtime_version: process.version,
+        runtime_version: `v${Bun.version}`,
         dc_version: `v${dcVersion}`,
         shark_version: `v${sharkVersion}`,
         cpu: `${os.cpus()[0].model}`
